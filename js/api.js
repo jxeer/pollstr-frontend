33a21f20bab4268068b4df1cdcbba0de6598ac72
julianXeer <julian.xeer@mattereum.com>
2023-01-06 16:32:01 -0500

    fixed swiper path

---
+++ js/api.js
@@ -0,0 +1,15 @@
+var buttonOne = document.querySelector('headingTwo');
+
+
+/*
+
+User clicks buttonOne
+event listner invokes a function
+function sends fetch request to Ruby Server
+
+Ruby server received fetch request
+responds to request by
+
+JS receives response and renders onto HTML
+
+*/
