e211d930c4dec0da43820bd244981c5d87b328aa
julianXeer <julian.xeer@mattereum.com>
2023-01-07 03:54:02 -0500

    results pages

--- js/api.js
+++ js/api.js
@@ -1,13 +1,61 @@
-// document.querySelector("panel-group").addEventListener("click", function() {
-//     if headerOne (yes)
-//     else (no)
-// }
-//
+import Chartkick from 'chartkick';
+import 'chartkick/chart.js';
 
-fetch('localhost:3000')
-.then(response => response.json())
-.then(data => console.log(data));
 
+const backend_url = 'http://localhost:3000';
+	interface Vote {
+		id: number;
+		name: string;
+		answer: string;
+		poll_id: number;
+		created_at: string;
+		updated_at: string;
+	}
+
+	interface Poll {
+		id: number;
+		description: string;
+		created_at: string;
+		updated_at: string;
+		url: string;
+		votes: Vote[];
+	}
+
+
+  let polls: Poll[] = [];
+	let charts = new Map<number, Map<string, number>>();
+	$: {
+		for (const [id, answers] of charts) {
+			new Chartkick.PieChart(`chart-${id}`, [...[...answers]]);
+		}
+	}
+
+  onMount(async function () {
+		const response = await fetch(`${backend_url}/polls.json`);
+		const poll_list: Poll[] = await response.json();
+		const outcomes = await Promise.allSettled(
+			poll_list.map(({ id }) => fetch(`${backend_url}/polls/${id}.json`))
+		);
+		// validation *could* go here
+		for (const outcome of outcomes) {
+			if (outcome.status === 'fulfilled') {
+				const response = outcome.value;
+				polls.push(await response.json());
+			}
+			polls = polls;
+		}
+	});
+
+  function tallyVotes(poll: Poll) {
+		// validation *could* go here
+		const answers = new Map<string, number>();
+		for (const vote of poll.votes) {
+			answers.set(vote.answer, (answers.get(vote.answer) ?? 0) + 1);
+		}
+		charts.set(poll.id, answers);
+		charts = charts;
+		return answers;
+	}
 
 /*
 
---
+++ vendor/chartkick.js
@@ -0,0 +1,100 @@
+/*!
+ * Chartkick.js
+ * Create beautiful charts with one line of JavaScript
+ * https://github.com/ankane/chartkick.js
+ * v4.0.2
+ * MIT License
+ */
+
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Chartkick = factory());
+}(this, (function () { 'use strict';
+
+  function isArray(variable) {
+    return Object.prototype.toString.call(variable) === "[object Array]";
+  }
+
+  function isFunction(variable) {
+    return variable instanceof Function;
+  }
+
+  function isPlainObject(variable) {
+    // protect against prototype pollution, defense 2
+    return Object.prototype.toString.call(variable) === "[object Object]" && !isFunction(variable) && variable instanceof Object;
+  }
+
+  // https://github.com/madrobby/zepto/blob/master/src/zepto.js
+  function extend(target, source) {
+    var key;
+    for (key in source) {
+      // protect against prototype pollution, defense 1
+      if (key === "__proto__") { continue; }
+
+      if (isPlainObject(source[key]) || isArray(source[key])) {
+        if (isPlainObject(source[key]) && !isPlainObject(target[key])) {
+          target[key] = {};
+        }
+        if (isArray(source[key]) && !isArray(target[key])) {
+          target[key] = [];
+        }
+        extend(target[key], source[key]);
+      } else if (source[key] !== undefined) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  function merge(obj1, obj2) {
+    var target = {};
+    extend(target, obj1);
+    extend(target, obj2);
+    return target;
+  }
+
+  var DATE_PATTERN = /^(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)$/i;
+
+  function negativeValues(series) {
+    var i, j, data;
+    for (i = 0; i < series.length; i++) {
+      data = series[i].data;
+      for (j = 0; j < data.length; j++) {
+        if (data[j][1] < 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function toStr(n) {
+    return "" + n;
+  }
+
+  function toFloat(n) {
+    return parseFloat(n);
+  }
+
+  function toDate(n) {
+    var matches, year, month, day;
+    if (typeof n !== "object") {
+      if (typeof n === "number") {
+        n = new Date(n * 1000); // ms
+      } else {
+        n = toStr(n);
+        if ((matches = n.match(DATE_PATTERN))) {
+          year = parseInt(matches[1], 10);
+          month = parseInt(matches[3], 10) - 1;
+          day = parseInt(matches[5], 10);
+          return new Date(year, month, day);
+        } else {
+          // try our best to get the str into iso8601
+          // TODO be smarter about this
+          var str = n.replace(/ /, "T").replace(" ", "").replace("UTC", "Z");
+          // Date.parse returns milliseconds if valid and NaN if invalid
+          n = new Date(Date.parse(str) || n);
+        }
+      }
+    }
+    return n;... (20 KB left)
